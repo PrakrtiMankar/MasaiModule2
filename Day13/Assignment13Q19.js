6189d1989a74ff9f3ac0adc671d78fd1a73b9439
PrakrtiMankar <prakrtimankar@gmail.com>
2024-12-06 01:59:15 +0530

    Understanding Global and Function Execution Context

---
+++ Day13/Assignment13Q19.js
@@ -0,0 +1,14 @@
+let age = 23;
+
+function displayAge(value) {
+    console.log(value);
+}
+
+displayAge(age);
+
+function changeAge(newValue) {
+    age = newValue;
+    console.log(age);
+}
+
+changeAge(25);
